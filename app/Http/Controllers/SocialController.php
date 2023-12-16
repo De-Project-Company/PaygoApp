afdaf9e6cf9d79bb73e7af1ccd3039169bcd4efd
Jackson Jackson <kodedjackson@gmail.com>
2023-12-16 18:22:49 +0100

    fixing google auth

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -11,18 +11,18 @@ use Laravel\Socialite\Facades\Socialite;
 
 class SocialController extends Controller
 {
-    public function loginWithFacebook() 
+    public function loginWithFacebook()
     {
         return Socialite::driver('facebook')->redirect();
     }
 
-    public function facebookRedirect() 
+    public function facebookRedirect()
     {
         try {
-    
+
             $user = Socialite::driver('facebook')->user();
             $isUser = User::where('facebook_id', $user->id)->first();
-     
+
             if($isUser){
 
                 Auth::login($isUser);
@@ -36,11 +36,11 @@ class SocialController extends Controller
                     'fb_id' => $user->id,
                     'password' => Hash::make($user->password)
                 ]);
-    
+
                 Auth::login($createUser);
                 return redirect('/dashboard');
             }
-    
+
         } catch (Exception $exception) {
             dd($exception->getMessage());
         }
@@ -58,7 +58,7 @@ class SocialController extends Controller
             $user = Socialite::driver('google')->user();
             $isUser = User::where('google_id', $user->id)->first();
 
-            if($isUser){
+            if($user){
 
                 Auth::login($isUser);
                 return redirect('/dashboard');
