8778be69e13b1513edfe5a8810d8f2d334ed49d4
kwindeezah <hadizathmotunrayo@gmail.com>
2023-12-17 19:28:01 +0100

    Added Apple login configuration

--- config/services.php
+++ config/services.php
@@ -42,4 +42,10 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
+
+    'apple' => [
+        'client_id' => env('APPLE_CLIENT_ID'),
+        'client_secret' => env('APPLE_CLIENT_SECRET'),
+        'redirect' => env('APPLE_REDIRECT_URI'),
+    ],
 ];
