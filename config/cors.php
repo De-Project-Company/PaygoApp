2d4a63cb3ac238fc392ff7d9b0e52f202d012efe
kodedjackson <kodedjackson@gmail.com>
2024-08-04 20:44:27 +0100

    added new url for the CORS error 2

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*', 'http://localhost:5174', 'http://localhost:5173', 'https://paygo-startershouse.vercel.app/', 'https://paygo-startershouse.vercel.app/*'],
+    'allowed_origins' => ['*', 'http://localhost:5174', 'http://localhost:5173', 'http://localhost:5176', 'https://paygo-startershouse.vercel.app/', 'https://paygo-startershouse.vercel.app/*'],
 
     'allowed_origins_patterns' => ['/^http:\/\/localhost:\d+$/'],
 
